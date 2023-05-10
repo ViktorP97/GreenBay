3f908d4a279eb9200a67c2a0e1cc7a6602d54989
Viktor Prusak <viktor.prusak@gmail.com>
2023-05-10 20:51:54 +0200

    feat(): new class for exception

---
+++ greenBay/src/main/java/com/gfa/greenbay/exceptions/NotEnoughGreenBayDollarsException.java
@@ -0,0 +1,9 @@
+package com.gfa.greenbay.exceptions;
+
+public class NotEnoughGreenBayDollarsException extends RuntimeException {
+
+  public NotEnoughGreenBayDollarsException(String message) {
+    super(message);
+  }
+
+}
