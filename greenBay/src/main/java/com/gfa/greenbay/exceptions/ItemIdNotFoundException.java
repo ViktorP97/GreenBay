df3ce83f48f6e290f78ce0e50f6feeb6553ac246
Viktor Prusak <viktor.prusak@gmail.com>
2023-05-10 20:51:13 +0200

    feat(): new class for exception

---
+++ greenBay/src/main/java/com/gfa/greenbay/exceptions/ItemIdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gfa.greenbay.exceptions;
+
+public class ItemIdNotFoundException extends RuntimeException{
+
+  public ItemIdNotFoundException(String message) {
+    super(message);
+  }
+}
