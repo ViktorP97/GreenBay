d8e75c2e1b3351166eb9f798200424c09429d212
Viktor Prusak <viktor.prusak@gmail.com>
2023-05-10 20:51:38 +0200

    feat(): new class for exception

---
+++ greenBay/src/main/java/com/gfa/greenbay/exceptions/MissingParameterException.java
@@ -0,0 +1,7 @@
+package com.gfa.greenbay.exceptions;
+
+public class MissingParameterException extends RuntimeException {
+  public MissingParameterException(String message) {
+    super(message);
+  }
+}
