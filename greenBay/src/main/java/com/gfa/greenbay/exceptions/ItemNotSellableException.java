37415cbace6d791c301492ad2266338767d151d3
Viktor Prusak <viktor.prusak@gmail.com>
2023-05-10 20:51:27 +0200

    feat(): new class for exception

---
+++ greenBay/src/main/java/com/gfa/greenbay/exceptions/ItemNotSellableException.java
@@ -0,0 +1,8 @@
+package com.gfa.greenbay.exceptions;
+
+public class ItemNotSellableException extends RuntimeException {
+
+  public ItemNotSellableException(String message) {
+    super(message);
+  }
+}
